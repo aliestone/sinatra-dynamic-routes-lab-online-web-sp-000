12927d514c00ec1ef56239eca05917f16b0cdc6e
Alie Stone <helloaliestone@gmail.com>
2020-12-15 23:23:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,7 +1,6 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
 
   get "/say/:number/:phrase" do
   end
@@ -30,6 +29,6 @@ class App < Sinatra::Base
   end
 
   get "/say/:word1/:word2/:word3/:word4/:word5" do
-    "#{params[:word1]} + #{params [:word2]} + #{params: [word3]} + #{params [:word4]} + #{params[:word5]}."
+    "#{params[:word1]} + #{params[:word2]} + #{params[word3]} + #{params[:word4]} + #{params[:word5]}."
   end
 end
